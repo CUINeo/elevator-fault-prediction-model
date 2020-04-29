0d1b4ef18e4e20802f58ddd1858b7aee437f10e6
Neo <32625025+StephenNeo@users.noreply.github.com>
2020-04-29 14:35:45 +0800

    测试模型真实性能

--- test_real_performance.py
+++ test_real_performance.py
@@ -10,7 +10,7 @@ from utils import get_previous_diff_date
 # port=3306
 # database='dt_yc'
 
-def test_real_performance(date):
+def test_real_performance(method_name, date, window=1):
     csv_file_name = 'features/' + date + '-feature.csv'
     csv_file = open(csv_file_name, 'r')
     reader = csv.reader(csv_file)
@@ -25,12 +25,13 @@ def test_real_performance(date):
 
         cnt = cnt + 1
         if cnt % 10000 == 0:
-            print('预测特征读取中，已完成' + str(cnt) + '条')
+            print('电梯特征读取中，已完成' + str(cnt) + '条')
 
     predict_X = [[float(i) if i.replace('.', '', 1).isdigit() else 0 for i in j] for j in predict_X]
     predict_X = np.asarray(predict_X, dtype=np.float32)
 
-    clf = load('knn.joblib')
+    joblib_name = method_name + '.joblib'
+    clf = load(joblib_name)
     proba = clf.predict_proba(predict_X)
     # res = clf.predict(predict_X)
 
@@ -45,15 +46,15 @@ def test_real_performance(date):
     for pos in pos_list:
         positive_tranid_set.append(tranid_set[pos])
     print('预测过程完成，得到正样本集合')
-        
-    future_date = get_previous_diff_date(date, -30)
+    
+    future_date = get_previous_diff_date(date, -window)
 
     conn = pymysql.connect(host='10.214.163.179', user='dt_yc', password='dt_yc123', port=3306, database='dt_yc')
     cursor = conn.cursor()
 
-    # 从数据库中得到previous_date到date时间段内发生故障电梯的tranid集合
+    # 从数据库中得到date到future_date时间段内发生故障电梯的tranid集合
     query = """
-            SELECT tranid FROM dt_yc.zt_dt_fault
+            SELECT DISTINCT tranid FROM dt_yc.zt_dt_fault
             WHERE form_create_time >= DATE_FORMAT(%s, '%%Y-%%m-%%d')
             AND form_create_time <= DATE_FORMAT(%s, '%%Y-%%m-%%d')
             """
@@ -83,6 +84,13 @@ def test_real_performance(date):
     recall = float(tp / (tp + fn))
     f1_score = float(2 * (recall * precision) / (recall + precision))
 
+    if method_name == 'rf':
+        print('Randon Forest:')
+    elif method_name == 'dt':
+        print('Decision Tree:')
+    elif method_name == 'knn':
+        print('KNN:')
+
     print('True Positive: ' + str(tp))
     print('False Positive: ' + str(fp))
     print('False Negative: ' + str(fn))
@@ -96,4 +104,7 @@ def test_real_performance(date):
     cursor.close()
     conn.close()
 
-test_real_performance('2020-02-02')
+date = '2020-04-20'
+test_real_performance('rf', date)
+test_real_performance('dt', date)
+test_real_performance('knn', date)
